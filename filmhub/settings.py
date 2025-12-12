0e7b7793556b77a028d0e60b0dd9773f594e6297
martinresplandy <martin.resplandy@ik.me>
2025-12-12 14:02:26 +0000

    Fixing

--- filmhub/settings.py
+++ filmhub/settings.py
@@ -84,15 +84,19 @@ WSGI_APPLICATION = "filmhub.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "filmhub_db"),
-        "USER": os.environ.get("DB_USER", "postgres"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "film123hub"),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
-        "PORT": os.environ.get("DB_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
+
+# Check to prevent running with incomplete settings if needed:
+if os.environ.get('DEBUG') == 'False' and not os.environ.get('DB_HOST'):
+    raise ValueError("DB_HOST environment variable is not set for production!")
 
 
 # Password validation
