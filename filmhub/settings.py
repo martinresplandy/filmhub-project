40a43d86db13a8e2f415f52015e07e6224a7c34e
martinresplandy <martin.resplandy@ik.me>
2025-12-12 15:34:10 +0000

    Fix: Final attempt - ensure frontend build is in repo

--- filmhub/settings.py
+++ filmhub/settings.py
@@ -46,9 +46,7 @@ INSTALLED_APPS = [
 # **********************************************
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    # WhiteNoise DOIT être ici (juste après SecurityMiddleware)
     'whitenoise.middleware.WhiteNoiseMiddleware', 
-    
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -167,6 +165,9 @@ if DEBUG:
 # CONFIGURATION WHITE NOISE
 # **********************************************
 STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
